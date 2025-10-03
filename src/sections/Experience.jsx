20f0af2fe6182808478b02c84341fad4ee107a91
Akwara Kelechi Emmanuel <137418423+Kellysabi@users.noreply.github.com>
2025-10-03 04:36:38 -0700

    Update Experience.jsx

--- src/sections/Experience.jsx
+++ src/sections/Experience.jsx
@@ -96,10 +96,11 @@ const Experience = () => {
       className="flex-center md:mt-40 mt-20 section-padding xl:px-0"
     >
       <div className="w-full h-full md:px-20 px-5">
-      <TitleHeader
+<TitleHeader
   title="Career & Academic Background"
   sub="💼🎓 My Professional and Educational Journey"
 />
+
 
         <div className="mt-32 relative">
           <div className="relative z-50 xl:space-y-32 space-y-10">
